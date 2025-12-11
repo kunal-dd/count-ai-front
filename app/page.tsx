6b5c62af94f45dce0fd5302a51ca70dfb75f6b94
kunal-dd <kunaldholiya@gmail.com>
2025-12-12 02:06:24 +0530

    feat: Redirect to agent

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,5 @@
-import { Sidebar } from "@/components/sidebar"
-import { PageHeader } from "@/components/page-header"
+import { redirect } from 'next/navigation'
 
-export default function AgentPage() {
-  return (
-    <div className="flex h-screen bg-background">
-      <Sidebar />
-      <div className="flex-1 flex flex-col overflow-hidden">
-        <PageHeader title="Agent" />
-
-        <div className="flex-1 overflow-auto p-6">{/* Empty agent page */}</div>
-      </div>
-    </div>
-  )
+export default function Home() {
+  redirect('/agent')
 }
